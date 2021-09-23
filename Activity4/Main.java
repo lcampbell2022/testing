90cc84e73e2ed8dc64661d8bf0435fd12da86e95
Lauren Campbell <lauren@Laurens-MacBook-Pro-2.local>
2021-09-23 09:57:28 -0500

    Edited Main.java

--- Activity4/Main.java
+++ Activity4/Main.java
@@ -2,5 +2,6 @@ public class Main{
   public static void main(String[] args){
     System.out.println("Lauren");
     System.out.println("Campbell");
+    System.out.println("lcampbell@sjs.org");
   }
 }
